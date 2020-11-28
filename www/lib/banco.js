cb526cccc766cc8d696ce343e4b67e8e264cb1fb
Jorge-Edson <60950040+Jorge-Edson@users.noreply.github.com>
2020-11-28 11:01:03 +0900

    Adicionando Update e Delete.

--- www/lib/banco.js
+++ www/lib/banco.js
@@ -71,3 +71,76 @@ $(document).on('change', '#listaPessoas', function() {
   });
 });
 
+function habilitarCampos() {
+  $('#nome').prop("readonly", false);
+  $('#email').prop("readonly", false);
+  $('#senha').prop("readonly", false);
+}
+
+function desabilitarCampos() {
+  $('#nome').prop("readonly", true);
+  $('#email').prop("readonly", true);
+  $('#senha').prop("readonly", true);
+}
+
+$(document).on('click', '#editar', function() {
+  habilitarCampos();
+});
+
+$(document).on('click', '#cancelar', function() {
+  desabilitarCampos();
+
+  $('#codigo').val(''),
+  $('#nome').val(''),
+  $('#senha').val(''),
+  $('#email').val('')
+});
+
+//Update
+$(document).on('click', '#salvarEdit', function() {
+  var parametros = {
+    "codigo": $('#codigo').val(),
+    "nome": $('#nome').val(),
+    "senha": $('#senha').val(),
+    "email": $('#email').val()
+  };
+
+  $.ajax({
+    type: "post", //Como vou enviar os dados
+    url: "https://wordpress-online-2.000webhostapp.com/webservice/atualiza.php", //Para onde vou enviar os dados
+    data: parametros, //O que eu vou enviar
+    //Se der certo:
+    success: function(data) {
+      navigator.notification.alert(data);
+      location.reload();
+      desabilitarCampos();
+    },
+    //Se der errado:
+    error: function(data) {
+      navigator.notification.alert("Erro no cadastro.");
+    }
+  });
+});
+
+//Delete
+$(document).on('click', '#excluir', function() {
+  var parametros = {
+    "codigo": $('#codigo').val()
+  };
+
+  $.ajax({
+    type: "post", //Como vou enviar os dados
+    url: "https://wordpress-online-2.000webhostapp.com/webservice/delete.php", //Para onde vou enviar os dados
+    data: parametros, //O que eu vou enviar
+    //Se der certo:
+    success: function(data) {
+      navigator.notification.alert(data);
+      location.reload();
+      desabilitarCampos();
+    },
+    //Se der errado:
+    error: function(data) {
+      navigator.notification.alert("Erro no cadastro.");
+    }
+  });
+});
